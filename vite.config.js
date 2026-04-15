026dbf12af096c4750d7b8a39d11b05c027cb354
Santiago Vallejos <vallejossantiago1412@gmail.com>
2026-04-15 16:30:05 -0300

    Eliminación de React del proyecto

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import {
-    defineConfig
-} from 'vite';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.jsx',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        react(),
         tailwindcss(),
     ],
-    esbuild: {
-        jsx: 'automatic',
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
     },
 });
