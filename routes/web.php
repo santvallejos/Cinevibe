709ffd77c5d3c63d70e2b06357b917e9099c1ff9
Santiago Vallejos <vallejossantiago1412@gmail.com>
2026-04-15 18:32:23 -0300

    Instalación de tailwind y primeras vistas replicadas de los bocetos

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/movie', function () {
+    return view('movies.show');
+})->name('movies.show');
